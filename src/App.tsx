3ec5ab5884b99802b7769a038c723f1993335583
devilUpTech <emil.steiner98622@gmail.com>
2025-07-05 09:51:52 -0700

    Update app

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ const App: React.FC = () => {
   return (
     <DragDropContext onDragEnd={onDrogEnd}>
       <div className="App">
-        <span className="heading">Taskify</span>
+        <span className="heading">Todo</span>
         <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
         <TodoList todos={todos} setTodos={setTodos} completedTodos={completedTodos} setCompletedTodos={setCompletedTodos} />
       </div>
